08790a0046cbd1383d1a2474709c339aef8fd2b5
Sol Harter <92319512+Sol455@users.noreply.github.com>
2023-02-24 16:17:55 +0000

    updated camera app

--- Core/Src/camera_application.c
+++ Core/Src/camera_application.c
@@ -15,9 +15,14 @@ DMA2D_HandleTypeDef hdma2d_eval;
 uint32_t  *ptrLcd;
 uint8_t status = CAMERA_OK;
 uint8_t frameCounter;
+uint8_t jpeg_packet_counter = 0;
+uint64_t jpeg_frame_size = 0;
+uint32_t jpeg_packet_size;
 
 uint8_t cam_fb[CAM_FB_SIZE] __attribute__ ((section (".sdram"), aligned (4)));
 uint8_t lcd_fb[LCD_FB_SIZE] __attribute__ ((section (".sdram"), aligned (4)));
+
+
 
 bool frame_data_available = false;
 
@@ -46,12 +51,23 @@ void initialiseCapture(void) {
     //BSP_CAMERA_Init(CAMERA_R640x480);
 	BSP_CAMERA_Init(CAMERA_R320x240);
 	BSP_CAMERA_ContinuousStart(cam_fb);
+    //jpeg_packet_size= *((uint32_t *)0x4002642c);
+}
+
+BSP_CAMERA_LineEventCallback(void) {
+	jpeg_packet_counter++;
+	//printf("\ngot packet\n");
 }
 
 BSP_CAMERA_VsyncEventCallback(void) {
 	LCD_DMA_Transfer_RGBTOARGB8888((uint32_t *)cam_fb, (uint32_t *)lcd_fb);
 	frameCounter++;
 	frame_data_available = true;
+	//jpeg_frame_size = ()jpeg_packet_counter * jpeg_packet_size;
+	printf("\nPACKET PER FRAME:  %i\n", jpeg_packet_counter);
+	printf("\nPACKET packet size:  %i\n", jpeg_packet_size);
+	printf("\nJPEG FRAME SIZE: %i\n", jpeg_frame_size);
+	jpeg_packet_counter = 0;
 }
 
 
